4a3f044b246d2fed9c9ce01169c1c56ad2d6162e
Yasitha Pandithawatta <yasitha4@gmail.com>
2016-10-05 22:24:07 +0800

    Force to use https

--- public/index.php
+++ public/index.php
@@ -7,6 +7,12 @@
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
+if ($_SERVER['HTTPS'] != "on") {
+    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit;
+}
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
